a064b76b83a5b6f1453f367e89219137ace8a843
ThanhBr <thanhmt>
2024-07-04 10:20:11 +0700

    reject mantine ui

--- src/app/medicine/[slug]/page.tsx
+++ src/app/medicine/[slug]/page.tsx
@@ -1,4 +1,5 @@
 const Slug = () => {
+
     return (
         <>
             Slug
--- src/app/medicine/page.tsx
+++ src/app/medicine/page.tsx
@@ -1,19 +1,9 @@
 'use client'
-import { useDisclosure } from '@mantine/hooks';
-import { Drawer, Button } from '@mantine/core';
 
 const Page = () => {
-  const [opened, { open, close }] = useDisclosure(false);
   return (
     <div>
         Medicine
-        <Button variant="filled">Button</Button>
-
-        <Drawer opened={opened} onClose={close} title="Authentication" position='right'>
-          {/* Drawer content */}
-        </Drawer>
-
-        <Button onClick={open}>Open Drawer</Button>
     </div>
   )
 }
