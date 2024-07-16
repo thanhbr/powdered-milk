e440c9a1858ebf64a88f5483fdaff2cb09f8c6f5
ThanhBr <thanhmt>
2024-07-16 14:21:50 +0700

    add routing metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,23 @@
-export default function DetailsProduct({
-  params,
-}: {
+import { Metadata } from "next"
+
+type Props = {
   params: {
-    productId: string
+    productId: string;
   }
-}) {
+}
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Product ${params.productId}`);
+    }, 100)
+  })
+  return {
+    title: `${title}`
+  }
+}
+
+
+export default function DetailsProduct({ params }: Props) {
   return <div>DetailsProduct: {params.productId}</div>
 }
