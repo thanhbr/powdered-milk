59c231f133056058a3580d4e14c4316431a767af
ThanhBr <thanhmt>
2024-07-16 16:16:06 +0700

    handling errors in layouts

--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -1,4 +1,15 @@
+"use client";
+
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default function ProductDetailsLayout({ children }: { children: React.ReactNode }) {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error loading product");
+  }
+
   return (
     <>
       <h2>Features products</h2>
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { notFound } from "next/navigation";
 
 function getRandomInt(count: number) {
