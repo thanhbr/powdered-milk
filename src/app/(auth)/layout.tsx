2dca15778f487ec932367fff8ee8b1b41b3ddbc9
ThanhBr <thanhmt>
2024-07-16 14:59:57 +0700

    add active link

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLink = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot password", href: "/forgot-password" }
+]
+
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  console.log('pathname', pathname);
+
+  return (
+    <div>
+      {navLink.map(link => {
+        const isActive = pathname?.startsWith(link.href)
+        return (<Link key={link.name} href={link.href} style={isActive ? {color: "blue"} : {color: "green"}}>
+          {link.name}
+        </Link>)
+      })}
+      {children}
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <main>Powdered Milk</main>;
+  return <main>
+    Powdered Milk
+  </main>;
 }
