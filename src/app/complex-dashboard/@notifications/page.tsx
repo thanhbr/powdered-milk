2346f84494290d98df6f68a1ec8a0bc070270439
ThanhBr <thanhmt>
2024-07-17 14:32:21 +0700

    unmatched routes

--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -1,5 +1,11 @@
 import Card from "@/components/card/page";
+import Link from "next/link";
 
 export default async function Notifications() {
-  return <Card>Notifications</Card>;
+  return (
+    <Card>
+      <div>Notifications</div>
+      <Link href={"/complex-dashboard/archived"}>Archived</Link>
+    </Card>
+  );
 }
