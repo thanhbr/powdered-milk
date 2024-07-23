160392b268f57bd4e5f203894722e45fe97df5f7
ThanhBr <thanhmt>
2024-07-23 13:12:41 +0700

    add logo page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icons/logo.svg" type="image/png" sizes="32x32" />
+      </head>
       <body className={poppins.className} suppressHydrationWarning={true}>
         <header style={headerStyle}>
           <p>Header</p>
