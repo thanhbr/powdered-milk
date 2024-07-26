4c3246730172db37deee8c4520d40d95847b6c0c
ThanhBr <thanhmt>
2024-07-26 17:03:36 +0700

    clean code

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,28 +1,22 @@
 "use client";
-import { API, API_URL } from "@/constants";
+import { API, API_GRAPHQL_URL, HEADERS_API, NAV_LINK } from "@/constants";
 import useSwr from "@/hooks/useSwr";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
-const navLink = [
-  { name: "Register", href: "/register" },
-  { name: "Login", href: "/login" },
-  { name: "Forgot password", href: "/forgot-password" },
-];
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
-  const { data, isLoading } = useSwr({
+  const { data } = useSwr({
     url: API.GET_PRODUCT,
     params: { min: 0, max: 10000000 },
   });
-  console.log("data", data);
-  console.log("isLoading", isLoading);
+  console.log("GET_PRODUCT", data);
 
   async function getCharacters() {
-    let results = await fetch('https://api-ecommerce.kidsplaza.io/graphql', {
+    let results = await fetch(`https://api-ecommerce.kidsplaza.io/graphql`, {
       method: 'POST',
 
       headers: {
@@ -30,6 +24,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
         "X-Api-Key": "IdLDYTgb2tzJnAU8XR8a6CwnQFPeVx71TbIuBUv5",
         "App-Cache-Id": "51dbf2026a9b3943984cb8589f6dacf03a562bce14c95484b98f043620cb8b16"
       },
+
 
       body: JSON.stringify({
         operationName: "InterestedProducts",
@@ -40,14 +35,14 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
     let products = await results.json();
     console.log("Interested Products", products.data)
   }
+
   useEffect(() => {
     getCharacters()
   }, [])
 
-
   return (
     <div>
-      {navLink.map((link) => {
+      {NAV_LINK.map((link) => {
         const isActive = pathname?.startsWith(link.href);
         return (
           <Link
@@ -63,358 +58,3 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
     </div>
   );
 }
-
-
-const FAKE_INTERESTED_PRODUCTS = `query InterestedProducts {
-    interestedProducts {
-        items {
-            ... on ConfigurableProduct {
-                ...ConfigurableProductBasic
-                ...ConfigurableProductOptions
-                ...ConfigurableProductVariants
-                ...ConfigurableProductPrice
-                ...ConfigurableProductImage
-                ...ConfigurableProductSpecialPrice
-                __typename
-            }
-            ... on SimpleProduct {
-                ...SimpleProductPrice
-                ...SimpleProductBasic
-                ...SimpleProductImage
-                ...SimpleProductSpecialPrice
-                __typename
-            }
-            __typename
-        }
-        __typename
-    }
-}
-fragment ConfigurableProductBasic on ConfigurableProduct {
-    id
-    sku
-    name
-    url_key
-    url_suffix
-    stock_status
-    brand
-    ratings_count
-    ratings_summary
-    has_gift
-    new_product
-    news_from_date
-    news_to_date
-    meta_title
-    meta_description
-    meta_keyword
-    canonical_url
-    __typename
-}
-fragment ConfigurableProductOptions on ConfigurableProduct {
-    configurable_options {
-        attribute_code
-        attribute_id
-        product_id
-        id
-        label
-        position
-        use_default
-        values {
-            default_label
-            label
-            store_label
-            use_default_value
-            value_index
-            __typename
-        }
-        __typename
-    }
-    __typename
-}
-fragment ConfigurableProductVariants on ConfigurableProduct {
-    variants {
-        attributes {
-            code
-            label
-            value_index
-            __typename
-        }
-        product {
-            id
-            sku
-            name
-            url_key
-            url_suffix
-            stock_status
-            brand
-            ratings_count
-            ratings_summary
-            media_gallery {
-                disabled
-                label
-                position
-                url
-                __typename
-            }
-            thumbnail {
-                disabled
-                label
-                position
-                url
-                __typename
-            }
-            small_image {
-                disabled
-                label
-                position
-                url
-                __typename
-            }
-            price_range {
-                maximum_price {
-                    discount {
-                        amount_off
-                        percent_off
-                        __typename
-                    }
-                    final_price {
-                        currency
-                        value
-                        __typename
-                    }
-                    fixed_product_taxes {
-                        amount {
-                            currency
-                            value
-                            __typename
-                        }
-                        label
-                        __typename
-                    }
-                    regular_price {
-                        currency
-                        value
-                        __typename
-                    }
-                    __typename
-                }
-                minimum_price {
-                    discount {
-                        amount_off
-                        percent_off
-                        __typename
-                    }
-                    final_price {
-                        currency
-                        value
-                        __typename
-                    }
-                    fixed_product_taxes {
-                        amount {
-                            currency
-                            value
-                            __typename
-                        }
-                        label
-                        __typename
-                    }
-                    regular_price {
-                        currency
-                        value
-                        __typename
-                    }
-                    __typename
-                }
-                __typename
-            }
-            special_from_date
-            special_price
-            special_to_date
-            __typename
-        }
-        __typename
-    }
-    __typename
-}
-fragment ConfigurableProductPrice on ConfigurableProduct {
-    price_range {
-        maximum_price {
-            discount {
-                amount_off
-                percent_off
-                __typename
-            }
-            final_price {
-                currency
-                value
-                __typename
-            }
-            fixed_product_taxes {
-                amount {
-                    currency
-                    value
-                    __typename
-                }
-                label
-                __typename
-            }
-            regular_price {
-                currency
-                value
-                __typename
-            }
-            __typename
-        }
-        minimum_price {
-            discount {
-                amount_off
-                percent_off
-                __typename
-            }
-            final_price {
-                currency
-                value
-                __typename
-            }
-            fixed_product_taxes {
-                amount {
-                    currency
-                    value
-                    __typename
-                }
-                label
-                __typename
-            }
-            regular_price {
-                currency
-                value
-                __typename
-            }
-            __typename
-        }
-        __typename
-    }
-    __typename
-}
-fragment ConfigurableProductImage on ConfigurableProduct {
-    thumbnail {
-        label
-        position
-        url
-        __typename
-    }
-    small_image {
-        label
-        position
-        url
-        __typename
-    }
-    __typename
-}
-fragment ConfigurableProductSpecialPrice on ConfigurableProduct {
-    special_from_date
-    special_price
-    special_to_date
-    __typename
-}
-fragment SimpleProductPrice on SimpleProduct {
-    price_range {
-        maximum_price {
-            discount {
-                amount_off
-                percent_off
-                __typename
-            }
-            final_price {
-                currency
-                value
-                __typename
-            }
-            fixed_product_taxes {
-                amount {
-                    currency
-                    value
-                    __typename
-                }
-                label
-                __typename
-            }
-            regular_price {
-                currency
-                value
-                __typename
-            }
-            __typename
-        }
-        minimum_price {
-            discount {
-                amount_off
-                percent_off
-                __typename
-            }
-            final_price {
-                currency
-                value
-                __typename
-            }
-            fixed_product_taxes {
-                amount {
-                    currency
-                    value
-                    __typename
-                }
-                label
-                __typename
-            }
-            regular_price {
-                currency
-                value
-                __typename
-            }
-            __typename
-        }
-        __typename
-    }
-    __typename
-}
-fragment SimpleProductBasic on SimpleProduct {
-    id
-    sku
-    name
-    url_key
-    url_suffix
-    stock_status
-    brand
-    ratings_count
-    ratings_summary
-    has_gift
-    new_product
-    news_from_date
-    news_to_date
-    meta_title
-    meta_description
-    meta_keyword
-    canonical_url
-    __typename
-}
-fragment SimpleProductImage on SimpleProduct {
-    thumbnail {
-        label
-        position
-        url
-        __typename
-    }
-    small_image {
-        label
-        position
-        url
-        __typename
-    }
-    __typename
-}
-fragment SimpleProductSpecialPrice on SimpleProduct {
-    special_from_date
-    special_price
-    special_to_date
-    __typename
-}
-`
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,23 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+export const API_GRAPHQL_URL = process.env.NEXT_PUBLIC_API_GRAPHQL_URL;
+export const API_GRAPHQL_APP_ID = process.env.NEXT_PUBLIC_API_GRAPHQL_APP_ID;
+export const API_GRAPHQL_KEY = process.env.NEXT_PUBLIC_API_GRAPHQL_KEY;
+
 export const API = {
   GET_PRODUCT: `${API_URL}/product/filter/all`,
   GET_PRODUCT_DETAIL: `${API_URL}/product`,
 };
+
+export const HEADERS_API = {
+  "Content-Type": "application/json",
+  "X-Api-Key": `${API_GRAPHQL_APP_ID}`,
+  "App-Cache-Id": `${API_GRAPHQL_APP_ID}`
+}
+
+export const NAV_LINK = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot password", href: "/forgot-password" },
+];
