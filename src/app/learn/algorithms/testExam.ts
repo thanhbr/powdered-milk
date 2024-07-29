887acb2d0a89b37d751029c4ccccd50f814fa564
ThanhBr <thanhmt>
2024-07-29 15:05:51 +0700

    recursive binary search

--- src/app/learn/algorithms/testExam.ts
+++ src/app/learn/algorithms/testExam.ts
@@ -30,11 +30,27 @@ function searchElement(numbers, target) {
   return -1;
 };
 
+function recursiveBinarySearch(arr, target) {
+  return searchBinary(arr, target, 0, arr.length - 1);
+}
+
+function searchBinary(arr, target, leftIndex, rightIndex) {
+  if(leftIndex > rightIndex) return -1;
+
+  const middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+  if(arr[middleIndex] === target) return middleIndex;
+
+  if(arr[middleIndex] > target) {
+    return searchBinary(arr, target, 0, middleIndex - 1)
+  } else return searchBinary(arr, target, middleIndex + 1, arr.length)
+}
+
 const arr = Array.from({ length: 100000001 }, (_, i) => i);
 console.time('result')
 // const result = searchElement(arr, 100000000);
 // const result = arr.find(item => item === 10000000)
-const result = binarySearch(arr, 100000000);
+// const result = binarySearch(arr, 100000000);
+const result = recursiveBinarySearch(arr, 100000000);
 console.timeEnd('result')
 
 console.log(result)
