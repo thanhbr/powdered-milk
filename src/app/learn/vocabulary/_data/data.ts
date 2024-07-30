1dbe6c517274b38c667e599d08df21534b3a7721
ThanhBr <thanhmt>
2024-07-30 13:51:23 +0700

    add vocabulary

--- src/app/learn/vocabulary/_data/data.ts
+++ src/app/learn/vocabulary/_data/data.ts
@@ -1,14 +1,20 @@
+import { v4 as id } from 'uuid';
+
 export const VOCABULARY = [
-  {id: 1, en: "abide by", vi: "tuân thủ / tuân theo", type: "v"},
-  {id: 2, en: "agreement", vi: "sự thỏa thuận hợp đồng", type: "n"},
-  {id: 3, en: "assurance", vi: "sự đảm bảo(n) / đảm bảo(v)", type: "n/v"},
-  {id: 4, en: "cancellation", vi: "sự hủy bỏ / chấm dứt", type: "n"},
-  {id: 5, en: "determine", vi: "xác định", type: "v"},
-  {id: 6, en: "engage", vi: "tham dự", type: "v"},
-  {id: 7, en: "establish", vi: "thành lập / thiết lập", type: "v"},
-  {id: 8, en: "obligate", vi: "bắt buộc", type: "v"},
-  {id: 9, en: "party", vi: "bên (tham gia hợp đồng)", type: "n"},
-  {id: 10, en: "provision", vi: "sự cung cấp / chu cấp điều khoản", type: "n"},
-  {id: 11, en: "resolve", vi: "giải quyết / quyết định", type: "v"},
-  {id: 12, en: "specific", vi: "cụ thể", type: "adj"}
+  {id: id(), en: "abide by", vi: "tuân thủ / tuân theo", type: "v"},
+  {id: id(), en: "agreement", vi: "sự thỏa thuận hợp đồng", type: "n"},
+  {id: id(), en: "assurance", vi: "sự đảm bảo(n) / đảm bảo(v)", type: "n/v"},
+  {id: id(), en: "cancellation", vi: "sự hủy bỏ / chấm dứt", type: "n"},
+  {id: id(), en: "determine", vi: "xác định", type: "v"},
+  {id: id(), en: "engage", vi: "tham dự", type: "v"},
+  {id: id(), en: "establish", vi: "thành lập / thiết lập", type: "v"},
+  {id: id(), en: "obligate", vi: "bắt buộc", type: "v"},
+  {id: id(), en: "party", vi: "bên (tham gia hợp đồng)", type: "n"},
+  {id: id(), en: "provision", vi: "sự cung cấp / chu cấp điều khoản", type: "n"},
+  {id: id(), en: "resolve", vi: "giải quyết / quyết định", type: "v"},
+  {id: id(), en: "specific", vi: "cụ thể", type: "adj"},
+  {id: id(), en: "agenda", vi: "chương trình nghị sự", type: "n"},
+  {id: id(), en: "innovation", vi: "sự đổi mới", type: "n"},
+  {id: id(), en: "introduction", vi: "sự giới thiệu", type: "n"},
+  {id: id(), en: "on-demand", vi: "theo yêu cầu", type: "n"},
 ]
