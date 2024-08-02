844a58e0dd5a9fab5778feb056fd9ccb7f756555
ThanhBr <thanhmt>
2024-08-02 13:11:18 +0700

    update page vocabulary

--- src/app/learn/vocabulary/page.tsx
+++ src/app/learn/vocabulary/page.tsx
@@ -13,6 +13,7 @@ export default function Page() {
   const [vocabulary, setVocabulary] = useState<any>({});
   const [error, setError] = useState(false);
   const [correct, setCorrect] = useState(false);
+  const [hide, setHide] = useState(true);
   const inputRef = useRef<any>(null);
 
   useEffect(() => setVocabulary(randomVocabulary), []);
@@ -25,6 +26,7 @@ export default function Page() {
       setShowList(false);
       setError(false);
       setCorrect(true);
+      setHide(true);
       inputRef.current.value = "";
       setTimeout(() => setCorrect(false), 2000);
     } else {
@@ -39,8 +41,10 @@ export default function Page() {
     inputRef.current.value = "";
   }
 
+  const handleHide = () => setHide(pre => !pre)
+
   const styleDiv = {
-    border: `1px solid ${error ? "red" : "black"}`,
+    border: `2px solid ${error ? "red" : correct ? "green" : "black"}`,
     width: "fit-content",
     margin: "16px 0"
   };
@@ -64,7 +68,10 @@ export default function Page() {
           <div style={styleDiv}>
             <input ref={inputRef} placeholder="enter english" style={styleInput} />
           </div>
-          {correct && <strong>Correct</strong>}
+          <div style={{display: "flex", gap: 8}}>
+            <button type="button" onClick={handleHide}>{hide ? "Show" : "Hide"}</button>
+            {!hide && <p>{vocabulary.en}</p>}
+          </div>
         </form>
         <div style={{ marginTop: 32 }}>
           <button onClick={() => setShowList((pre) => !pre)}>Show list</button>
@@ -72,7 +79,7 @@ export default function Page() {
             {showList &&
               VOCABULARY.map((vocal) => (
                 <li key={vocal.id}>
-                  <strong>{vocal.en} {vocal.type ? `(${vocal.type})` : ""}</strong>: {vocal.vi}
+                  <strong>{vocal.en}{vocal.type ? `(${vocal.type})` : ""}</strong>: {vocal.vi}
                 </li>
               ))}
           </ol>
