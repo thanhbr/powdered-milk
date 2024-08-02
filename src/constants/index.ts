471c852bafd8a233705cdedc111f8f78642c3bda
ThanhBr <thanhmt>
2024-08-02 17:02:27 +0700

    change fetch data server side

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,7 +6,7 @@ export const API_GRAPHQL_APP_ID = process.env.NEXT_PUBLIC_API_GRAPHQL_APP_ID;
 export const API_GRAPHQL_KEY = process.env.NEXT_PUBLIC_API_GRAPHQL_KEY;
 
 export const API = {
-  GET_PRODUCT: `${API_URL}/product/filter/all`,
+  GET_PRODUCT: `${API_URL}/product/filter/all`, // params: { min: 0, max: 10000000, per_page: 100, url_key_v2: "sua-cho-be", get_brand_info: true }
   GET_PRODUCT_DETAIL: `${API_URL}/product`,
 };
 
