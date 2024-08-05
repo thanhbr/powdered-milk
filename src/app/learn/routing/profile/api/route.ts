41e9220178e00c57e757f43423ce60b18c56aaa5
ThanhBr <thanhmt>
2024-08-05 09:54:35 +0700

    cookies in route handles

--- src/app/learn/routing/profile/api/route.ts
+++ src/app/learn/routing/profile/api/route.ts
@@ -1,15 +1,23 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const headerList = headers();
 
-  // console.log('headerList', headerList)
-  // console.log('authorization', requestHeaders.get("Authorization"))
+  cookies().set("resultsPerPage", 20);
+  const theme = request.cookies.get("theme");
+
+  console.log("headerList", headerList);
+  console.log("authorization", requestHeaders.get("Authorization"));
+
+  console.log("theme", theme);
+  console.log("results per page", cookies().get("resultsPerPage"));
+
   return new Response("<h1>Profile API data</h1>", {
     headers: {
-      "Content-Type": "text/html"
-    }
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
   });
 }
