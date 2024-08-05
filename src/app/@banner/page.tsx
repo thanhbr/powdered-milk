684234496b53e0a40f2ddd361734df7835e34961
ThanhBr <thanhmt>
2024-08-05 15:51:59 +0700

    style home page

---
+++ src/app/@banner/page.tsx
@@ -0,0 +1,11 @@
+export default async function BannerPage() {
+  return (
+    <>
+      <div>
+        <p className="col-span-3 text-2xl font-bold">
+          Banner
+        </p>
+      </div>
+    </>
+  );
+}
