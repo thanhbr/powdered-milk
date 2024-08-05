f644db0ce2c355452459aff42b86f439f442b130
ThanhBr <thanhmt>
2024-08-05 08:21:17 +0700

    change background card product

--- src/components/card/ProductCard.tsx
+++ src/components/card/ProductCard.tsx
@@ -17,7 +17,7 @@ interface ProductCardProps {
 export default function ProductCard(props: ProductCardProps): JSX.Element {
   return (
     <div className="bg-white product-card-wrapper flex h-full flex-col drop-shadow-lg rounded-xl">
-      <article className="product-card shadow-product-card relative h-full overflow-hidden p-2 transition-all duration-200 md:p-3.5 rounded-xl">
+      <article className="bg-white product-card shadow-product-card relative h-full overflow-hidden p-2 transition-all duration-200 md:p-3.5 rounded-xl">
         <div className="absolute right-0 top-0 z-[8] flex flex-col gap-2.5">
           <div className="flex flex-col space-y-1" />
         </div>
