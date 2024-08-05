ef42b78cc9afcb34b8c238c3a25508f4a46f5fee
ThanhBr <thanhmt>
2024-08-05 11:47:21 +0700

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get("theme");
+  if(!themePreference) {
+    response.cookies.set("theme", "dark");
+  }
+  return response;
+
+  // if(request.nextUrl.pathname === "/profile") {
+  //   return NextResponse.rewrite(new URL("/hello", request.url))
+  // }
+
+  // return NextResponse.redirect(new URL('/', request.url));
+}
+
+// export const config = {
+//   matcher: "/profile"
+// }
