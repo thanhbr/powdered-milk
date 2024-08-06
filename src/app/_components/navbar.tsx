3e7cafca75223923b7819b9baa210c3c43c07b3a
ThanhBr <thanhmt>
2024-08-06 09:36:06 +0700

    style navbar and scroll page

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "./sidebar";
 
 export default function Navbar() {
   return (
-    <div className="p-4 h-full flex items-center bg-teal-600 shadow-xl">
+    <div className="sticky top-0 w-full z-[9999] p-4 flex items-center bg-teal-600 shadow-xl">
       {/* <MobileSidebar /> */}
       <Sidebar />
     </div>
