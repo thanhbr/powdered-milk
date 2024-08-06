d7be85c2b8cce6ec2e3a6793793dfb91973f9fe8
ThanhBr <thanhmt>
2024-08-06 11:28:54 +0700

    add error and loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,10 +5,10 @@ export default function Loading() {
     <div>
       <p>Loading</p>
       <div className="flex flex-col space-y-3">
-        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+        <Skeleton className="h-[125px] w-[250px] rounded-xl bg-teal-100" />
         <div className="space-y-2">
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
+          <Skeleton className="h-4 w-[250px] bg-teal-100" />
+          <Skeleton className="h-4 w-[200px] bg-teal-100" />
         </div>
       </div>
     </div>
