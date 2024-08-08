565127ee4acdfb4af3771e6806a068d21aaf6e4e
ThanhBr <thanhmt>
2024-08-08 16:52:45 +0700

    loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,17 +1,17 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 export default function Loading() {
   return (
     <div className="container">
       <div className="text-center m-auto flex justify-center mt-40">
-        {/* <Image
+        <Image
           src={"/icons/Infinity@200px-200px.gif"}
           alt="loading..."
           width={200}
           height={200}
           className="w-auto h-auto rounded-[50%]"
           priority
-        /> */}
+        />
       </div>
     </div>
   );
