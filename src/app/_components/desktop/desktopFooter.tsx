26fa508decf75b3ba7625f3defc62fcdef00d066
ThanhBr <thanhmt>
2024-08-26 15:23:19 +0700

    add item footer

---
+++ src/app/_components/desktop/desktopFooter.tsx
@@ -0,0 +1,3 @@
+export default function DesktopFooter() {
+  return <footer></footer>;
+}
---
+++ src/app/_components/mobile/mobileFooter.tsx
@@ -0,0 +1,11 @@
+const MobileFooterItems = [
+  { id: 1, name: "Trang chủ", href: "/"},
+  { id: 2, name: "Tìm kiếm", href: "/search"},
+  { id: 3, name: "Khuyến mãi", href: "/promotion"},
+  { id: 4, name: "Giỏ hàng", href: "/order"},
+  { id: 5, name: "Tài khoản", href: "/account"},
+]
+
+export default function MobileFooter() {
+  return <footer></footer>;
+}
